9e2398d1a59a0a82a31482af3729ba1c17347ad7
Rick Marvin <rmarvin@nd.edu>
2024-07-12 17:40:44 -0400

    Initial Commit

---
+++ Piazza/PiazzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiazzaApp.swift
+//  Piazza
+//
+//  Created by Rick Marvin on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PiazzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
